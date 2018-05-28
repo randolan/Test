8bcd747f2ab0efcd84ab6a5768ef6ed343579e4a
angela-rph <34378157+angela-rph@users.noreply.github.com>
2018-05-28 16:04:49 +0200

    Update testlinux.py

--- testlinux.py
+++ testlinux.py
@@ -1,1 +1,1 @@
-arduino --board STM32:stm32:Nucleo_64:pnum=NUCLEO_F303RE --verify /usr/local/share/arduino/examples/01.Basics/Blink/Blink.ino
+./arduino --board STM32:stm32:Nucleo_64:pnum=NUCLEO_F303RE --verify /usr/local/share/arduino/examples/01.Basics/Blink/Blink.ino
